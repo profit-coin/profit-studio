f5fb00419366f7b2f590c1f8b8380ae6afae326d
Manfies <okunevden97@gmail.com>
2024-06-20 01:37:41 +0200

    chore: updated login page

---
+++ src/components/common/Animation/Lottie.tsx
@@ -0,0 +1,4 @@
+import dynamic from 'next/dynamic';
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
+
+export default Lottie;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,8 +3,10 @@ import { useTranslation } from 'next-i18next'
 import Button from '@/components/common/Button/Button'
 import Heading from '@/components/common/Heading/Heading'
 import Text from '@/components/common/Text/Text'
-import Coin from '../../../public/coin.svg'
+// import Coin from '../../../public/coin.svg'
 import styles from './index.module.scss'
+import Lottie from 'lottie-react'
+import coinAnimation from '../../../public/animation/coin.json'
 
 interface CreateAccountFlowProps {
   theme: 'light' | 'dark'
@@ -21,17 +23,21 @@ function CreateAccountFlow({ theme = 'light', onAccountCreate }: CreateAccountFl
   }
 
   return (
-    <div className={styles.flow}>
-      <Coin className={styles.logo} />
-      <Heading theme={theme} size="h1">
-        {t('flows.createAccount.title')}
-      </Heading>
-      <Text color="primary" size="large">
-        {t('flows.createAccount.description')}
-      </Text>
-      <Button onClick={handleCreateAccount} variant="primary">
-        {t('flows.createAccount.button')}
-      </Button>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <Lottie animationData={coinAnimation} className={styles.logo} />
+        <Heading theme={theme} size="h1">
+          {t('flows.createAccount.title')}
+        </Heading>
+        <Text color="primary" size="large">
+          {t('flows.createAccount.description')}
+        </Text>
+      </div>
+      <div className={styles.footer}>
+        <Button onClick={handleCreateAccount} variant="primary" isFullWidth>
+          {t('flows.createAccount.button')}
+        </Button>
+      </div>
     </div>
   );
 }
