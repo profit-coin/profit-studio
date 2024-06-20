b0379d87a249c3b15d1420c2ed129a67f57b0b3a
Manfies <okunevden97@gmail.com>
2024-06-20 03:32:55 +0200

    feat: user - create new user mutation

---
+++ src/data/user.ts
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import appConfig from '@/config/appConfig';
+
+interface CreateUserPayload {
+  telegramId: number;
+  username: string;
+  firstName?: string;
+  lastName?: string;
+  language?: string;
+}
+
+export const useCreateUserMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation<unknown, unknown, CreateUserPayload>({
+    mutationFn: async (data) => {
+      const response = await axios.post(`${appConfig.apiBaseUrl}/studio/user`, data);
+      console.log(response);
+    }
+  });
+}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,32 +1,50 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'next-i18next'
 import Button from '@/components/common/Button/Button'
 import Heading from '@/components/common/Heading/Heading'
 import Text from '@/components/common/Text/Text'
-// import Coin from '../../../public/coin.svg'
 import styles from './index.module.scss'
 import Lottie from 'lottie-react'
 import coinAnimation from '../../../public/animation/coin.json'
+import { useCreateUserMutation } from '@/data/user'
+import {TelegramUser} from '@/data/telegram'
 
-interface CreateAccountFlowProps {
-  theme: 'light' | 'dark'
-  onAccountCreate: () => void
-}
-
-function CreateAccountFlow({ theme = 'light', onAccountCreate }: CreateAccountFlowProps) {
+function CreateAccountFlow () {
   const { t } = useTranslation();
 
-  useEffect(() => {}, [])
+  const [telegramUser, setTelegramUser] = useState<TelegramUser | null>(null)
+  const createUserMutation = useCreateUserMutation();
 
-  const handleCreateAccount = () => {
-    onAccountCreate()
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.initDataUnsafe) {
+      window.Telegram.WebApp.BackButton.hide();
+      const query = new URLSearchParams(window.Telegram.WebApp.initData)
+      const user = query.get('user')
+
+      if (user) {
+        setTelegramUser(JSON.parse(user))
+      }
+    }
+  }, [])
+
+  const handleCreateAccount = async () => {
+    if (!telegramUser) {
+      return;
+    }
+    await createUserMutation.mutateAsync({
+      telegramId: telegramUser.id,
+      username: telegramUser.username,
+      firstName: telegramUser.first_name,
+      lastName: telegramUser.last_name,
+      language: telegramUser.language_code,
+    });
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <Lottie animationData={coinAnimation} className={styles.logo} />
-        <Heading theme={theme} size="h1">
+        <Heading size="h1">
           {t('flows.createAccount.title')}
         </Heading>
         <Text color="primary" size="large">
